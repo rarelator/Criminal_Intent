99bb466abc6df73094886d19d306bcf1fa7c6c0c
Ross Anthony R Relator <ross.relator@uconn.edu>
2023-11-27 10:18:57 -0500

    DatePicker lecture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     kapt("androidx.room:room-compiler:2.6.0")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
--- app/src/main/java/com/example/criminal_intent/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminal_intent/CrimeDetailFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.criminal_intent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
@@ -65,9 +67,9 @@ class CrimeDetailFragment : Fragment() {
 //            crimeDate.setOnClickListener{ view : View ->
 //                crimeDate.setText(crime.isSolved.toString())
 //            }
-            crimeDate.apply{
-                isEnabled = false
-            }
+//            crimeDate.apply{
+//                isEnabled = false
+//            }
 
             crimeSolved.setOnCheckedChangeListener{ _, isChecked ->
                 crimeDetailViewModel.updateCrime{oldCrime ->
@@ -83,6 +85,14 @@ class CrimeDetailFragment : Fragment() {
                     }
                 }
             }
+
+            setFragmentResultListener(
+                DatePickerFragment.REQUEST_KEY_DATE
+            ) {
+                _, bundle ->
+                val newDate = bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+                crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
+            }
         }
     }
 
@@ -92,6 +102,9 @@ class CrimeDetailFragment : Fragment() {
                 crimeTitle.setText(crime.title)
             }
             crimeDate.text = crime.date.toString()
+            crimeDate.setOnClickListener {
+                findNavController().navigate(CrimeDetailFragmentDirections.selectDate(crime.date))
+            }
             crimeSolved.isChecked = crime.isSolved
         }
     }
